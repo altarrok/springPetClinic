7bf4f5c3db1fa3f544a36670deb97f321731da27
altay batuhan <altaybatuhanmail@gmail.com>
2020-04-28 09:51:50 -0700

    Added PetType to bootstrap, closes #7

--- pet-clinic-web/src/main/java/altayo/spring/petclinic/bootstrap/DataBoot.java
+++ pet-clinic-web/src/main/java/altayo/spring/petclinic/bootstrap/DataBoot.java
@@ -1,8 +1,10 @@
 package altayo.spring.petclinic.bootstrap;
 
 import altayo.spring.petclinic.model.Owner;
+import altayo.spring.petclinic.model.PetType;
 import altayo.spring.petclinic.model.Vet;
 import altayo.spring.petclinic.services.OwnerService;
+import altayo.spring.petclinic.services.PetTypeService;
 import altayo.spring.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,26 @@ public class DataBoot implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataBoot(OwnerService ownerService, VetService vetService) {
+    public DataBoot(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Juice");
