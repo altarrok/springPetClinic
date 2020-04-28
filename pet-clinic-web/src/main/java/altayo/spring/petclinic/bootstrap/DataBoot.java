492217c00cef274f2cd2a62bfca8bdfed03a2296
altay batuhan <altaybatuhanmail@gmail.com>
2020-04-28 10:13:01 -0700

    Added Pets and contact info to owners on startup, closes #8

--- pet-clinic-web/src/main/java/altayo/spring/petclinic/bootstrap/DataBoot.java
+++ pet-clinic-web/src/main/java/altayo/spring/petclinic/bootstrap/DataBoot.java
@@ -1,6 +1,7 @@
 package altayo.spring.petclinic.bootstrap;
 
 import altayo.spring.petclinic.model.Owner;
+import altayo.spring.petclinic.model.Pet;
 import altayo.spring.petclinic.model.PetType;
 import altayo.spring.petclinic.model.Vet;
 import altayo.spring.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import altayo.spring.petclinic.services.PetTypeService;
 import altayo.spring.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataBoot implements CommandLineRunner {
@@ -27,27 +30,40 @@ public class DataBoot implements CommandLineRunner {
 
         PetType dog = new PetType();
         dog.setName("Dog");
-
         PetType savedDogPetType = petTypeService.save(dog);
 
         PetType cat = new PetType();
         cat.setName("Cat");
-
         PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Juice");
         owner1.setLastName("Brown");
+        owner1.setAddress("2323 Brisket St.");
+        owner1.setCity("Vancouver");
+        owner1.setTelephone("123 123 1212");
 
+        Pet pet1 = new Pet();
+        pet1.setName("Coo");
+        pet1.setPetType(savedDogPetType);
+        pet1.setBirthDate(LocalDate.now());
+        pet1.setOwner(owner1);
+        owner1.getPets().add(pet1);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Iona");
         owner2.setLastName("James");
+        owner2.setAddress("9847 Qtip St.");
+        owner2.setCity("Los Angeles");
+        owner2.setTelephone("758 826 8383");
 
+        Pet pet2 = new Pet();
+        pet2.setName("Tippo");
+        pet2.setPetType(savedCatPetType);
+        pet2.setBirthDate(LocalDate.now());
+        pet2.setOwner(owner2);
         ownerService.save(owner2);
-
-        System.out.println("## Loaded Owners");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Ukra");
@@ -61,6 +77,6 @@ public class DataBoot implements CommandLineRunner {
 
         vetService.save(vet2);
 
-        System.out.println("## Loaded Vets");
+        System.out.println("## Bootstrap successful");
     }
 }
